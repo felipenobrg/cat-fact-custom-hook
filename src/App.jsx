149f6065c9c2270609a839256cc7b37d2b7c8e42
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-04-19 14:52:19 -0300

    adding styles and gif

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Cat } from "./Cat";
 
--- src/Cat.jsx
+++ src/Cat.jsx
@@ -1,13 +1,30 @@
-import { useGetCat } from "./useGetCat"
+import { useGetCat } from "./useGetCat";
+import { useState } from "react";
 
 export const Cat = () => {
-     const {data, isLoading, refetch} = useGetCat()
-     if (isLoading) return <h1>Loading...</h1>
-    return (
-        <div>
-            <button onClick={refetch}>refetch</button>
-            <h1> {data?.fact} </h1>
-        </div>
-    )
-}
+  const [imageUrl, setImageUrl] = useState(
+    "https://cdn2.thecatapi.com/images/4io.gif"
+  );
+  const { data, isLoading, refetch } = useGetCat();
+  if (isLoading) return <h1>Loading...</h1>;
 
+  const handleRefetch = () => {
+    refetch();
+    setImageUrl(image);
+  };
+
+  return (
+    <>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Cat Facts</h5>
+          <p className="card-text">{data?.fact}</p>
+          <a href="#" className="btn btn-primary" onClick={handleRefetch}>
+            Generate a new fact
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
